e2362a76e80d5fd8991e57db50f9e570c7e8ff56
Ruslan Yarmolenko <yarm.rus@yandex.ru>
2022-07-20 16:02:06 +0700

    fix: иправить ошибки, вторая итерация

--- script/script.js
+++ script/script.js
@@ -1,37 +1,29 @@
 // Ищем основные переменные для открытия popup
 let modal = document.querySelector('.popup'); // Ищу элемент popup'а
 let buttonModalOff = document.querySelector('.popup__close'); // Ищу кнопку закрытия popup'а
-let buttonSubmit = document.querySelector('.popup__button'); // Ищу кнопку "Сохранить" в popup
 let buttonModalOn = document.querySelector('.profile__info-button'); // Ищу кнопку открытия popup'а
-
-
-// Делаем открывание popup
-buttonModalOn.addEventListener('click', function(){
-  modal.classList.add('popup_opened');
-});
-
-buttonModalOff.addEventListener('click', function(){
-  modal.classList.remove('popup_opened');
-});
-
-buttonSubmit.addEventListener('click', function(){
-  modal.classList.remove('popup_opened');
-});
-
-// Делаем редактирование информации имени и о себе
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_place_top');
 let jobInput = document.querySelector('.popup__input_place_bottom');
+let infoTitle = document.querySelector('.profile__info-title');
+let infoSubtitle = document.querySelector('.profile__info-subtitle');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
+function openModal() {
+  modal.classList.add('popup_opened');
+}
 
-  let infoTitle = document.querySelector('.profile__info-title');
-  let infoSubtitle = document.querySelector('.profile__info-subtitle');
+function closeModal() {
+  modal.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
 
   infoTitle.textContent = `${nameInput.value}`;
   infoSubtitle.textContent = `${jobInput.value}`;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
+formElement.addEventListener('submit', closeModal);
+buttonModalOn.addEventListener("click", openModal);
+buttonModalOff.addEventListener('click', closeModal);
