0c8620a8d8bcc7996a6bb41239c96cfa17c75e49
yarmolenko-ruslan <yarm.rus@yandex.ru>
2022-07-30 12:30:41 +0700

    feat: Новый попап, функция его открытия и закрытия

--- script/script.js
+++ script/script.js
@@ -1,12 +1,35 @@
+// Запишим все елементы в объект
+const selectors = {
+  modal: '.popup-rename',
+  close: '.popup-rename__close',
+  open: '.profile__info-button',
+  form: '.popup-rename__form',
+  nameInput: '.popup__input_place_top',
+  jobInput: '.popup__input_place_bottom',
+  title: '.profile__info-title',
+  subtitle: '.profile__info-subtitle',
+
+  modalCard: '.popup-card',
+  closeCard: '.popup-card__close',
+  openCard: '.profile__button',
+  formCard: '.popup-card__form'
+}
+
 // Ищем основные переменные для открытия popup
-let modal = document.querySelector('.popup'); // Ищу элемент popup'а
-let buttonModalOff = document.querySelector('.popup__close'); // Ищу кнопку закрытия popup'а
-let buttonModalOn = document.querySelector('.profile__info-button'); // Ищу кнопку открытия popup'а
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_place_top');
-let jobInput = document.querySelector('.popup__input_place_bottom');
-let infoTitle = document.querySelector('.profile__info-title');
-let infoSubtitle = document.querySelector('.profile__info-subtitle');
+const modal = document.querySelector(selectors.modal);
+const buttonModalOff = document.querySelector(selectors.close);
+const buttonModalOn = document.querySelector(selectors.open);
+const formElement = document.querySelector(selectors.form);
+const nameInput = document.querySelector(selectors.nameInput);
+const jobInput = document.querySelector(selectors.jobInput);
+const infoTitle = document.querySelector(selectors.title);
+const infoSubtitle = document.querySelector(selectors.subtitle);
+
+const modalCard = document.querySelector(selectors.modalCard);
+const closeCardBtn = document.querySelector(selectors.closeCard);
+const openCardBtn = document.querySelector(selectors.openCard);
+const formCard = document.querySelector(selectors.formCard);
+
 
 function openModal() {
   modal.classList.add('popup_opened');
@@ -15,8 +38,16 @@ function openModal() {
   jobInput.value = infoSubtitle.textContent;
 }
 
+function openCard() {
+  modalCard.classList.add('popup_opened');
+}
+
 function closeModal() {
   modal.classList.remove('popup_opened');
+}
+
+function closeCard() {
+  modalCard.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -31,3 +62,6 @@ function formSubmitHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 buttonModalOn.addEventListener("click", openModal);
 buttonModalOff.addEventListener('click', closeModal);
+
+openCardBtn.addEventListener('click', openCard);
+closeCardBtn.addEventListener('click', closeCard);
